f296cfe1c317e146e55a7ba02d576bd61a99a436
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-09-30 23:30:39 +0100

    refactor of previous commit

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,27 +1,26 @@
 describe 'User Stories' do
-
-  it 'so spaceships dock at spaceports, instruct a spaceship to dock' do
-    spaceport = Spaceport.new(36)
-    spaceship = Spaceship.new
-    allow(spaceport).to receive(:security_alert?).and_return false
-    expect { spaceport.dock(spaceship) }.not_to raise_error
-  end
-
-  it 'so spaceships un-dock from spaceports, instruct a spaceship to un-dock' do
-    spaceport = Spaceport.new(36)
-    spaceship = Spaceship.new
-    expect { spaceport.release(spaceship) }.not_to raise_error
-  end
-
-  context 'when spacestation is full' do
-    it 'does not allow spaceships to dock' do
-      spaceport = Spaceport.new(36)
-      spaceship = Spaceship.new
+  let(:spaceport) { Spaceport.new(36) }
+  let(:spaceship) { Spaceship.new }
+  context 'when security alert inactive' do
+    before do
       allow(spaceport).to receive(:security_alert?).and_return false
-      36.times do
-        spaceport.dock(spaceship)
+    end
+
+    it 'so spaceships dock at spaceports, instruct a spaceship to dock' do
+      expect { spaceport.dock(spaceship) }.not_to raise_error
+    end
+
+    it 'so spaceships un-dock from spaceports, instruct a spaceship to un-dock' do
+      expect { spaceport.release(spaceship) }.not_to raise_error
+    end
+
+    context 'when spacestation is full' do
+      it 'does not allow spaceships to dock' do
+        36.times do
+          spaceport.dock(spaceship)
+        end
+        expect { spaceport.dock(spaceship) }.to raise_error 'Cannot dock spaceship: spaceport full.'
       end
-      expect { spaceport.dock(spaceship) }.to raise_error 'Cannot dock spaceship: spaceport full.'
     end
   end
 
@@ -29,11 +28,10 @@ describe 'User Stories' do
   # So that I can ensure safe release procedures
   # I want to prevent spaceships from docking or releasing during security alerts
   context 'when security alert is active' do
-    it 'does not allow spaceships to dock' do
-      spaceport = Spaceport.new(36)
-      spaceship = Spaceship.new
-
+    before do
       allow(spaceport).to receive(:security_alert?).and_return true
+    end
+    it 'does not allow spaceships to dock' do
       expect { spaceport.dock(spaceship) }.to raise_error 'Cannot dock spaceship: security alert active.'
     end
   end
